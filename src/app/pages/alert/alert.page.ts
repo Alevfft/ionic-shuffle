82c330d7e76bfca7a27b76fe6599f09d724348ff
AkeMM <aomdmy1@outlook.com>
2024-09-22 16:53:55 -0600

    proyecto ionic

---
+++ src/app/pages/alert/alert.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.page.html',
+  styleUrls: ['./alert.page.scss'],
+})
+export class AlertPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
